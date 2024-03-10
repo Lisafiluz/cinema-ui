26a062296a4b012bb8b2e8d55189bd45c5983ff1
liluz <liluz@ebay.com>
2024-03-11 00:36:14 +0200

    add change

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
+const API_BASE_URL = 'http://localhost:8080';
 const HomePage = () => {
 	const [movies, setMovies] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
@@ -11,12 +12,13 @@ const HomePage = () => {
 		const fetchMovies = async () => {
 			try {
 				// Replace with your actual API endpoint if different
-				const response = await fetch('/movie/popular');
+				const response = await fetch(`${API_BASE_URL}/movie/popular`);
 				if (!response.ok) {
 					throw new Error('Something went wrong!');
 				}
 				const data = await response.json();
-				setMovies(data.results); // Assuming the API returns an object with a 'results' array
+				console.log(data)
+				setMovies(data.entities);
 				setIsLoading(false);
 			} catch (err) {
 				setError(err.message);
@@ -41,7 +43,7 @@ const HomePage = () => {
 				<div className="container mt-4">
 					<div className="row">
 						{movies.map((movie) => (
-								<div className="col-md-4 mb-3" key={movie.id}>
+								<div className="col-md-4 mb-3" key={movie.movieId}>
 									<MovieCard movie={movie} />
 								</div>
 						))}
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const MovieCard = ({ movie }) => {
 	// Construct the image URL (you may need to prepend a base URL for images)
-	const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+	const imageUrl = `https://image.tmdb.org/t/p/w500${movie.picUrl}`;
 
 	return (
 			<div className="card" style={{ width: '18rem' }}>
 				<img src={imageUrl} className="card-img-top" alt={movie.title} />
 				<div className="card-body">
 					<h5 className="card-title">{movie.title}</h5>
-					<p className="card-text">{movie.overview}</p>
-					<p className="card-text">Rating: {movie.vote_average}</p>
+					<p className="card-text">{movie.description}</p>
+					<p className="card-text">Rating: {movie.review}</p>
+					<p className="card-text">Genre: {movie.genre}</p>
+					<p className="card-text">Release Date: {new Date(movie.releaseDate).getFullYear()}</p>
 					{/* Add more movie details as needed */}
 				</div>
 			</div>
